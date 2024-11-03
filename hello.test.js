bfbf9e5d09a4599e92c2c021f10e04bf70eb99e9
hbelay24 <haiman.belay@outlook.com>
2024-11-03 18:49:56 -0500

    Text update

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from the Capital!");
+    expect(hello.hello()).toEqual("Hello World from Spain!");
   });
 });
